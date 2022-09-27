1a3602458c4a3e6fa7ef35f0a1b7f54243e62919
TakanoriShima <quark2galaxy@gmail.com>
2022-09-27 05:41:33 +0000

    認証と認可の実装まで書いた

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TestController;
 
@@ -17,10 +17,20 @@ use App\Http\Controllers\TestController;
 |
 */
 
-Route::get('/', [AuthController::class, 'index']);
+// タスク管理システム
+Route::get('/', [AuthController::class, 'index'])->name('front.index');
+Route::post('/login', [AuthController::class, 'login']);
+// 認可処理
+Route::middleware(['auth'])->group(function () {
+    Route::get('/task/list', [TaskController::class, 'list']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
+
+
+
+// テスト用
 Route::get('/welcome', [WelcomeController::class, 'index']);
 Route::get('/welcome/second', [WelcomeController::class, 'second']);
-Route::get('/task/list', [TaskController::class, 'list']);
+// form入力テスト用
 Route::get('/test', [TestController::class, 'index']);
 Route::post('/test/input', [TestController::class, 'input']);
-Route::post('/login', [AuthController::class, 'login']);
