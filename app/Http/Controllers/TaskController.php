10e3750f562c5d761db8602706332ab3eeeb9b01
TakanoriShima <quark2galaxy@gmail.com>
2022-09-27 07:56:03 +0000

    タスクの編集を書いた

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,6 +7,9 @@ use App\Http\Requests\TaskRegisterPostRequest;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Task as TaskModel;
 
+use Illuminate\Http\Request;
+
+
 class TaskController extends Controller
 {
     /**
@@ -25,10 +28,20 @@ class TaskController extends Controller
                          ->orderBy('period')
                          ->orderBy('created_at')
                          ->paginate($per_page);
-                        // ->get();        //
+                        // ->get();
+/*
+$sql = TaskModel::where('user_id', Auth::id())
+                 ->orderBy('priority', 'DESC')
+                 ->orderBy('period')
+                 ->orderBy('created_at')
+                 ->toSql();
+//echo "<pre>\n"; var_dump($sql, $list); exit;
+var_dump($sql);
+*/
+        //
         return view('task.list', ['list' => $list]);
     }
-    
+
     /**
      * タスクの新規登録
      */
@@ -59,26 +72,91 @@ class TaskController extends Controller
         //
         return redirect('/task/list');
     }
-    
+
     /**
      * タスクの詳細閲覧
      */
     public function detail($task_id)
     {
+        //
+        return $this->singleTaskRender($task_id, 'task.detail');
+    }
+
+    /**
+     * タスクの編集画面表示
+     */
+    public function edit($task_id)
+    {
+        // task_idのレコードを取得する(引数で取得)
+        // テンプレートに「取得したレコード」の情報を渡す
+        return $this->singleTaskRender($task_id, 'task.edit');
+    }
+
+    /**
+     * 「単一のタスク」Modelの取得
+     */
+    protected function getTaskModel($task_id)
+    {
         // task_idのレコードを取得する
         $task = TaskModel::find($task_id);
         if ($task === null) {
-            return redirect('/task/list');
+            return null;
         }
         // 本人以外のタスクならNGとする
         if ($task->user_id !== Auth::id()) {
+            return null;
+        }
+        //
+        return $task;
+    }
+
+    /**
+     * 「単一のタスク」の表示
+     */
+    protected function singleTaskRender($task_id, $template_name)
+    {
+        // task_idのレコードを取得する
+        $task = $this->getTaskModel($task_id);
+        if ($task === null) {
             return redirect('/task/list');
         }
 
-// var_dump($task); exit;
-        
         // テンプレートに「取得したレコード」の情報を渡す
-        return view('task.detail', ['task' => $task]);
+        return view($template_name, ['task' => $task]);
     }
-    
+
+    /**
+     * タスクの編集処理
+     */
+    public function editSave(TaskRegisterPostRequest $request, $task_id)
+    {
+        // formからの情報を取得する(validate済みのデータの取得)
+        $datum = $request->validated();
+
+        // task_idのレコードを取得する
+        $task = $this->getTaskModel($task_id);
+        if ($task === null) {
+            return redirect('/task/list');
+        }
+
+        // レコードの内容をUPDATEする
+        $task->name = $datum['name'];
+        $task->period = $datum['period'];
+        $task->detail = $datum['detail'];
+        $task->priority = $datum['priority'];
+/*
+        // 可変変数を使った書き方(参考)
+        foreach($datum as $k => $v) {
+            $task->$k = $v;
+        }
+*/
+        // レコードを更新
+        $task->save();
+
+        // タスク編集成功
+        $request->session()->flash('front.task_edit_success', true);
+        // 詳細閲覧画面にリダイレクトする
+        return redirect(route('detail', ['task_id' => $task->id]));
+    }
+
 }
