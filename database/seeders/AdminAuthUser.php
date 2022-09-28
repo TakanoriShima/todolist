06b23646bac42ee2a465989c4cd3fd8fbb604a9f
TakanoriShima <quark2galaxy@gmail.com>
2022-09-28 05:48:21 +0000

    管理画面の認証と認可を書いた

---
+++ database/seeders/AdminAuthUser.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+use Illuminate\Database\Seeder;
+
+class AdminAuthUser extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('admin_users')->insert([
+            'login_id' => 'hogemin',
+            'password' => Hash::make('pass'),
+        ]);
+        DB::table('admin_users')->insert([
+            'login_id' => 'admin',
+            'password' => Hash::make('admin'),
+        ]);
+    }
+}
